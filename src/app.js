87a88a6416b07a72923c93fc781e04664555c353
Ashadozzaman <shovoua@gmail.com>
2023-12-06 23:19:56 +0600

    Developed Protfolio Site

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const hamburger = document.querySelector('#hamburger')
 const menu = document.querySelector('#menu')
+const body = document.querySelector('body')
+const dark = document.querySelector('#dark')
+const Hlinks = document.querySelectorAll('.Hlink')
 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle('hidden')
     hamburger.classList.toggle('bg-white')
+})
+
+dark.addEventListener("click", () => {
+    body.classList.toggle('dark')
+})
+
+Hlinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle('hidden')
+        hamburger.classList.toggle('bg-white')
+    })
 })
